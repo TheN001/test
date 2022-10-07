fab191eabde209c586aa989e326d0382ce92e17f
Giovany <106202775+GiovanYCringe@users.noreply.github.com>
2022-10-06 21:58:26 -0300

    a50-common: boardconfig: changed clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,8 +91,9 @@ TARGET_KERNEL_CONFIG := a50_defconfig
 
 # Clang
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := azure
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-azure/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CLANG_VERSION := prelude
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 
 
 ## Keymaster
